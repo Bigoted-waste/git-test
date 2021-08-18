fc5b1af196a4cdc60f8eaea03d6ab949d643556f
cola <jn123977@163.com>
2021-08-18 10:21:06 +0800

    第二次提交

--- src/main/java/com/cola/git/GitTest.java
+++ src/main/java/com/cola/git/GitTest.java
@@ -3,5 +3,6 @@ package com.cola.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello gitHub!");
     }
 }
